2c2de82135a9aa0de74ed24358249d3a8553a6a0
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-06-30 10:13:08 +0800

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Ahmad Adib Bin Ahmad Rafi on 30/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
